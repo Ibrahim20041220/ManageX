22f628f524e8635ad6dc044cdea15c20f19be5b2
Ibrahim20041220 <brahim20041220@gmail.com>
2025-12-06 17:37:57 +0100

    add logoManageX

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -27,6 +28,10 @@ public class App extends Application {
                 getClass().getResource("/styles/signUp.css").toExternalForm());
 
         stage.setScene(scene);
+        stage.setTitle("ManageX");
+
+        Image icon = new Image(getClass().getResourceAsStream("/icons/logoManageX.png"), 300, 300, true, true);
+        stage.getIcons().add(icon);
         stage.show();
     }
 
